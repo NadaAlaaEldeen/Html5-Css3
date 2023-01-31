e7e5befd87928d8636f0b1d673549d7e3573dd73
‪Mariam Khaled ‬‏ <78925756+Marim99@users.noreply.github.com>
2023-01-31 14:45:46 +0200

    Delete Day2 directory

--- Day2/scripts/welcome.js
+++
@@ -1,5 +0,0 @@
-let user = document.querySelector("#welcome-user");
-let email = document.querySelector("#welcome-email");
-
-user.textContent = localStorage.getItem("username");
-email.textContent = localStorage.getItem("userEmail");
--- Day2/scripts/worker.js
+++
@@ -1,7 +0,0 @@
-this.onmessage = () => {
-  let sum = 0;
-  for (let i = 0; i < 10000000000; i++) {
-    sum += i;
-  }
-  this.postMessage(sum);
-};
